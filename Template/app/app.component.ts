e2b7472461b31149744abeec52e2d962b61b0955
Mintautas Kiulkys <mintautas0@gmail.com>
2017-04-03 01:18:37 +0300

    hg commit -m "Application startup files."

---
+++ Template/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>TEST</h1>'
+})
+export class AppComponent { }
+
+@Component({
+  selector: 'test',
+  template: '<h2>TEST</h2>'
+})
+export class AppComponent1 { }
+
---
+++ Template/app/main.ts
@@ -0,0 +1,8 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+import { AppComponent1 } from './app.component';
+
+bootstrap(AppComponent);
+bootstrap(AppComponent1);
